7dd05322ba42717cb5ac7456a036bb4ea271ac64
DiellezaHelshani <diellezahelshani@gmail.com>
2024-02-24 00:17:30 +0100

    added new networks and fixed icons

--- packages/extension-koni-ui/src/hooks/chain/useChainInfoWithState.tsx
+++ packages/extension-koni-ui/src/hooks/chain/useChainInfoWithState.tsx
@@ -3,7 +3,9 @@
 
 import { _ChainInfo, _ChainStatus } from '@subwallet/chain-list/types';
 import { _ChainState } from '@subwallet/extension-base/services/chain-service/types';
+import { CustomLogoMap } from '@subwallet/extension-koni-ui/extras/TokenMap';
 import { RootState } from '@subwallet/extension-koni-ui/stores';
+import { updateLogoMaps } from '@subwallet/extension-koni-ui/stores/utils';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -12,10 +14,24 @@ export type ChainInfoWithState = _ChainInfo & _ChainState;
 export default function useChainInfoWithState ({ filterStatus = true } = {} as {filterStatus?: boolean}): ChainInfoWithState[] {
   const chainInfoMap = useSelector((state: RootState) => state.chainStore.chainInfoMap);
   const chainStateMap = useSelector((state: RootState) => state.chainStore.chainStateMap);
+  const logoMaps = useSelector((state: RootState) => state.settings.logoMaps);
 
   const chainInfoList: ChainInfoWithState[] = useMemo(() => {
+    const updatedLogoMaps = { ...logoMaps };
     const rs = Object.values(chainInfoMap).map((item) => {
-      return { ...item, ...(chainStateMap[item.slug] || {}) };
+      const newItem = { ...item };
+
+      if (newItem.slug.includes('custom')) {
+        newItem.icon = CustomLogoMap[newItem.slug];
+        updatedLogoMaps.chainLogoMap = {
+          [newItem.slug]: CustomLogoMap[newItem.slug]
+        };
+        updateLogoMaps(updatedLogoMaps);
+      }
+
+      const newState = chainStateMap[newItem.slug] || {};
+
+      return { ...newItem, ...newState };
     });
 
     if (filterStatus) {
